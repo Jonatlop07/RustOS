f87c52351ff81b8c2e92ce2792600b067848579d
Jonathan Lopez Castellanos <jolopezca@unal.edu.co>
2022-06-22 19:26:18 -0500

    First steps

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+// This function is called on panic
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+fn main() {
+    println!("Hello, world!");
+}
